3386c66a1f6b0ae8d22a4837ad980dfb80201481
Santiago Jorda <sjorda@fi.uba.ar>
2020-06-04 18:15:18 -0300

    Eliminacion de cuerpo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { RootComponent } from './estructura/root/root.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './estructura/header/header.component';
-import { CuerpoComponent } from './estructura/cuerpo/cuerpo.component';
 import { FooterComponent } from './estructura/footer/footer.component';
 
 @NgModule({
   declarations: [
     RootComponent,
     HeaderComponent,
-    CuerpoComponent,
     FooterComponent
   ],
   imports: [
