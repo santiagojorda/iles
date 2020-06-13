730dec6e9bae897599454549174b872c11913ad4
Santiago Jorda <sjorda@fi.uba.ar>
2020-06-13 02:52:32 -0300

    Titulos

--- src/app/paginas/inicio/inicio.component.ts
+++ src/app/paginas/inicio/inicio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-inicio',
@@ -7,11 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
-  constructor() { 
-    
+  constructor(private title: Title) {
+
   }
 
   ngOnInit(): void {
+    this.title.setTitle('ILES :: Instituto Latinoamericano de Estudios Sociales');
   }
-
 }
